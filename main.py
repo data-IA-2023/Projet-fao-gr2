8182360d1719e837095f55a2f81a69f9eb7f1867
AlexandreBoult <alex.boult@orange.fr>
2023-12-19 17:32:22 +0100

    updated the panda and main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv('fao_2013/FAOSTAT_2013_population.csv')
-#filetot=df['Value'].sum()*1000
-#chinapop = df.drop(df.index[32:36])['Value'].sum()*1000
-#df.index[df['Country'] == "China"].tolist()
-totpop = df.drop(df.index[df['Country'] == "China"].tolist())['Value'].sum()*1000
-#print(df.to_string())
-#totpop = df['Value'].sum()*1000
-print(totpop)
-#print(filetot-chinapop)
+pop = pd.read_csv('fao_2013/FAOSTAT_2013_population.csv')
+vg = pd.read_csv('fao_2013/FAOSTAT_2013_vegetal.csv')
+
+vg1p = vg.drop(vg.index[vg['Élément'] != "Disponibilité de protéines en quantité (g/personne/jour)"].tolist())['Pays']
+vg1pr = vg.drop(vg.index[vg['Élément'] != "Disponibilité de protéines en quantité (g/personne/jour)"].tolist())['Produit']
+vg1e = vg.drop(vg.index[vg['Élément'] != "Disponibilité de protéines en quantité (g/personne/jour)"].tolist())['Élément']
+vg1v = vg.drop(vg.index[vg['Élément'] != "Disponibilité de protéines en quantité (g/personne/jour)"].tolist())['Valeur']
+vg2v = vg.drop(vg.index[vg['Élément'] != "Disponibilité alimentaire (Kcal/personne/jour)"].tolist())['Valeur']
+vg2e = vg.drop(vg.index[vg['Élément'] != "Disponibilité alimentaire (Kcal/personne/jour)"].tolist())['Élément']
+vg2p = vg.drop(vg.index[vg['Élément'] != "Disponibilité alimentaire (Kcal/personne/jour)"].tolist())['Pays']
+vg2pr = vg.drop(vg.index[vg['Élément'] != "Disponibilité alimentaire (Kcal/personne/jour)"].tolist())['Produit']
+
+country=pd.concat([pop['Country'],pop['Value']*1000],axis=1)
+
+dap=pd.concat([vg1p, vg1pr, vg1e, vg1v], axis=1)
+da=pd.concat([vg2p, vg2pr, vg2e, vg2v], axis=1)
+
+
+print(dap)
+print(country)
 #print(df.drop(df.index[32:36])['Country'].to_string())
