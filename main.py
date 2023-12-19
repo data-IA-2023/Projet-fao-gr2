6e2a191313f4a0d02bca047d2d0295e3807de143
AlexandreBoult <alex.boult@orange.fr>
2023-12-19 13:59:31 +0100

    question 1

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 df = pd.read_csv('fao_2013/FAOSTAT_2013_population.csv')
-filetot=df['Value'].sum()*1000
-chinapop = df.drop(df.index[32:36])['Value'].sum()*1000
-totpop = df.drop(df.index[-1])['Value'].sum()*1000
+#filetot=df['Value'].sum()*1000
+#chinapop = df.drop(df.index[32:36])['Value'].sum()*1000
+#df.index[df['Country'] == "China"].tolist()
+totpop = df.drop(df.index[df['Country'] == "China"].tolist())['Value'].sum()*1000
 #print(df.to_string())
 #totpop = df['Value'].sum()*1000
 print(totpop)
-print(filetot-chinapop)
+#print(filetot-chinapop)
 #print(df.drop(df.index[32:36])['Country'].to_string())
