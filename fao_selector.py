8f1ecb4a1f3199b2bfb33a8caaf284d015dde375
RTenaille <118723783+RTenaille@users.noreply.github.com>
2023-12-19 17:15:19 +0100

    premier jet

--- fao_selector.py
+++ fao_selector.py
@@ -1,5 +1,8 @@
 import pandas as pd
+df_population2013=pd.read_csv("fao_2013/FAOSTAT_2013_population.csv")
+df_animal2013=pd.read_csv("fao_2013/FAOSTAT_2013_animal.csv")
+df_cereal2013=pd.read_csv("fao_2013/FAOSTAT_2013_cereal.csv")
+df_sous_alimentation2013=pd.read_csv("fao_2013/FAOSTAT_2013_sous_alimentation.csv")
+df_vegetal2013=pd.read_csv("fao_2013/FAOSTAT_2013_vegetal.csv")
 
-data_pays = pd.read_csv('fao_2013\FAOSTAT_2013_population.csv')
-data_chine_drop = pd.DataFrame.drop()
-print(data_pays)
+tableau_population_animal = pd.merge(df_population2013,df_animal2013, how='outer', on='Code pays', indicator=True).query('_merge!="both"').drop(columns='_merge')
