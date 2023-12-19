728387a4038980d6feaa27c90b208a338c0ff109
AlexandreBoult <alex.boult@orange.fr>
2023-12-19 11:37:43 +0100

    updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-f = pd.read_csv('fao_2013/FAOSTAT_2013_population.csv')
-
-print(f.to_string()) 
+df = pd.read_csv('fao_2013/FAOSTAT_2013_population.csv')
+filetot=df['Value'].sum()*1000
+chinapop = df.drop(df.index[32:36])['Value'].sum()*1000
+totpop = df.drop(df.index[-1])['Value'].sum()*1000
+#print(df.to_string())
+#totpop = df['Value'].sum()*1000
+print(totpop)
+print(filetot-chinapop)
+#print(df.drop(df.index[32:36])['Country'].to_string())
