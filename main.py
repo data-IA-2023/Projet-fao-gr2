0580c9758cf536c14212d6cfcb7213e912feaaed
AlexandreBoult <alex.boult@orange.fr>
2023-12-20 16:29:06 +0100

    Traduction des noms des pays

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import pandas as pd
 import numpy as np
 
+tt = pd.read_csv('country_translation_table.csv')
 pop = pd.read_csv('fao_2013/FAOSTAT_2013_population.csv')
 vg = pd.read_csv('fao_2013/FAOSTAT_2013_vegetal.csv')
+countrypop=pd.concat([pop['Country'],pop['Value']*1000],axis=1)
 
-vg1p = vg.drop(vg.index[vg['Élément'] != "Disponibilité de protéines en quantité (g/personne/jour)"].tolist())['Pays']
-vg1pr = vg.drop(vg.index[vg['Élément'] != "Disponibilité de protéines en quantité (g/personne/jour)"].tolist())['Produit']
-vg1e = vg.drop(vg.index[vg['Élément'] != "Disponibilité de protéines en quantité (g/personne/jour)"].tolist())['Élément']
-vg1v = vg.drop(vg.index[vg['Élément'] != "Disponibilité de protéines en quantité (g/personne/jour)"].tolist())['Valeur']
-vg2v = vg.drop(vg.index[vg['Élément'] != "Disponibilité alimentaire (Kcal/personne/jour)"].tolist())['Valeur']
-vg2e = vg.drop(vg.index[vg['Élément'] != "Disponibilité alimentaire (Kcal/personne/jour)"].tolist())['Élément']
-vg2p = vg.drop(vg.index[vg['Élément'] != "Disponibilité alimentaire (Kcal/personne/jour)"].tolist())['Pays']
-vg2pr = vg.drop(vg.index[vg['Élément'] != "Disponibilité alimentaire (Kcal/personne/jour)"].tolist())['Produit']
 
-country=pd.concat([pop['Country'],pop['Value']*1000],axis=1)
+L=[]
+for i,Country in countrypop.iterrows():
+    a=tt.iloc[tt.index[tt["English"]==Country["Country"]].tolist()]['French']
+    L.append(a)
+countrypop=pd.concat([pd.concat(L,axis=0).reset_index(drop=True),pop['Value']*1000],axis=1)
 
-dap=pd.concat([vg1p, vg1pr, vg1e, vg1v], axis=1)
-da=pd.concat([vg2p, vg2pr, vg2e, vg2v], axis=1)
+countrypop = countrypop.rename(columns={'French': 'Pays', 'Value': 'Population'})
+
+
+
+
+vg1p = vg.drop(vg.index[vg['Élément'] != "Disponibilité de protéines en quantité (g/personne/jour)"])['Pays']
+vg1pr = vg.drop(vg.index[vg['Élément'] != "Disponibilité de protéines en quantité (g/personne/jour)"])['Produit']
+vg1e = vg.drop(vg.index[vg['Élément'] != "Disponibilité de protéines en quantité (g/personne/jour)"])['Élément']
+vg1v = vg.drop(vg.index[vg['Élément'] != "Disponibilité de protéines en quantité (g/personne/jour)"])['Valeur']
+vg2v = vg.drop(vg.index[vg['Élément'] != "Disponibilité alimentaire (Kcal/personne/jour)"])['Valeur']
+vg2e = vg.drop(vg.index[vg['Élément'] != "Disponibilité alimentaire (Kcal/personne/jour)"])['Élément']
+vg2p = vg.drop(vg.index[vg['Élément'] != "Disponibilité alimentaire (Kcal/personne/jour)"])['Pays']
+vg2pr = vg.drop(vg.index[vg['Élément'] != "Disponibilité alimentaire (Kcal/personne/jour)"])['Produit']
+
+
+dap=pd.concat([vg1p, vg1pr, vg1e, vg1v], axis=1).reset_index(drop=True)
+da=pd.concat([vg2p, vg2pr, vg2e, vg2v], axis=1).reset_index(drop=True)
+
+
 
 
 print(dap)
-print(country)
-#print(df.drop(df.index[32:36])['Country'].to_string())
+print(countrypop)#print(df.drop(df.index[32:36])['Country'].to_string())
