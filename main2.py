22c8afc4a7c090b6387273dee37e900aeafb5d8e
AlexandreBoult <alex.boult@orange.fr>
2024-01-09 13:18:15 +0100

    tableau 3d v2

--- main2.py
+++ main2.py
@@ -6,7 +6,7 @@ an = pd.read_csv('fao_2013/FAOSTAT_2013_animal.csv')
 
 nour=pd.concat([an,vg],axis=0).sort_values(by=['Code Pays','Code Produit']).reset_index(drop=True)
 pop_pays=pd.concat([pop['Country Code'],pop['Value']*1000],axis=1).rename(columns={"Country Code":"Code Pays","Value":"Population"})
-df_uni=nour.merge(pop_pays,how="left",on="Code Pays")[["Code Élément","Code Produit", "Code Pays","Population","Valeur"]]
+df_uni=nour.merge(pop_pays,on="Code Pays")[["Code Élément","Code Produit", "Code Pays","Population","Valeur"]]
 valeurs=pd.Series([nour[nour["Code Pays"]==i].pivot_table(values='Valeur', index='Code Produit', columns='Code Élément') for i in pop['Country Code']],index=pop['Country Code']).rename_axis("Code Pays")
 
 print(df_uni)
