04d60bab8420b7e869c83d9b2e815d1ce361c368
AlexandreBoult <alex.boult@orange.fr>
2024-01-09 13:26:01 +0100

    nouveaux tableaux

--- main2.py
+++ main2.py
@@ -8,6 +8,8 @@ nour=pd.concat([an,vg],axis=0).sort_values(by=['Code Pays','Code Produit']).rese
 pop_pays=pd.concat([pop['Country Code'],pop['Value']*1000],axis=1).rename(columns={"Country Code":"Code Pays","Value":"Population"})
 df_uni=nour.merge(pop_pays,on="Code Pays")[["Code Élément","Code Produit", "Code Pays","Population","Valeur"]]
 valeurs=pd.Series([nour[nour["Code Pays"]==i].pivot_table(values='Valeur', index='Code Produit', columns='Code Élément') for i in pop['Country Code']],index=pop['Country Code']).rename_axis("Code Pays")
+valeurs_moy=nour.pivot_table(values='Valeur', index='Code Produit', columns='Code Élément',aggfunc='mean')
+valeurs_tot=nour.pivot_table(values='Valeur', index='Code Produit', columns='Code Élément',aggfunc='sum')
 
 print(df_uni)
 print(valeurs)
